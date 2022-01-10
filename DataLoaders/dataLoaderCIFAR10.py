34855f27ec0dc5594ce19f0b44c66e8c52361967
rana-shahroz <ranashahrozkhan01@gmail.com>
2022-01-09 22:40:06 -0600

    Added DataLoader for CIFAR10

--- DataLoaders/dataLoaderCIFAR10.py
+++ DataLoaders/dataLoaderCIFAR10.py
@@ -11,6 +11,41 @@ def dataLoadCIFAR10(args):
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5), inplace=False)
     ])
+        
+    # Downloading the train and test sets.
+    dataTrain = datasets.CIFAR10(root = args.path_data,
+                                 train = True, 
+                                 download = True,
+                                 transform=transform
+                                 )
     
-    # TODO : ALSO write the denormalize function for debugging.
+    
+    dataTest = datasets.CIFAR10(root = args.path_data, 
+                                train = False,
+                                download = True,
+                                transform = transform
+                                )
+    
+    # Making the loaders for train and test data.
+    trainLoader = torch.utils.data.DataLoader(
+        dataset = dataTrain,
+        batch_size = args.batch_size,
+        shuffle = True,
+        num_workers = args.num_workers,
+        drop_last = True,
+        timeout = 0,
+        persistent_workers = False
+    )
+    
+    testLoader = torch.utils.data.DataLoader(
+        dataset = dataTest,
+        batch_size = args.batch_size,
+        shuffle = False,
+        num_workers = args.num_workers,
+        drop_last = True,
+        timeout = 0,
+        persistent_workers = False
+    )
+    
+    return trainLoader, testLoader
     
